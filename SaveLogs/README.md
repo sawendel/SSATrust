f044a315cd2853e4aa3425c64ca1ad7cdeef912a
Juan Guevara <juan.guevara@wundermanthompson.com>
2022-05-03 11:50:56 -0600

    add Function Lambda Proyect

---
+++ SaveLogs/README.md
@@ -0,0 +1,9 @@
+# Getting Started
+
+This project is to create the lambda function to store logs of the Enhancing Trust project, first install all dependencies by running the command:
+
+```
+npm install
+```
+
+Then, you must create a zip of the content, and import it into the `saveLogs` lambda function in aws
---
+++ SaveLogs/index.js
@@ -0,0 +1,40 @@
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb+srv://" + process.env.adminmbd + ":" + process.env.passwordmbd + "@ssatrust.rrthf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let cachedDb = null;
+
+async function connectToDatabase() {
+    if (cachedDb) {
+
+        return cachedDb;
+    }
+
+    const client = await MongoClient.connect(MONGODB_URI);
+    const db = await client.db('EnhancingTrust');
+    cachedDb = db;
+    return db
+}
+
+exports.handler = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const db = await connectToDatabase();
+
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+    var body = JSON.parse(event.body);
+    const inputLog = {
+        userId: body.userId,
+        eventType: body.eventType,
+        workflowId: body.workflowId,
+        templateName: body.templateName,
+        templateType: body.templateType,
+        date: date
+    };
+    const result = await db.collection("savedLogs").insertOne(inputLog);
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(result),
+    };
+    return response;
+};
